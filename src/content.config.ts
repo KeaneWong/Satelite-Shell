856fa95a43bb5db007496b70fdace56806914ccf
Keane Wong <keanewong913@gmail.com>
2025-07-19 17:01:51 -0700

    Add stormlight page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,32 +1,42 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import {glob} from 'astro/loaders';
+import {defineCollection, z} from 'astro:content';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
+    // Load Markdown and MDX files in the `src/content/blog/` directory.
+    loader: glob({base: './src/content/blog', pattern: '**/*.{md,mdx}'}),
+    // Type-check frontmatter using a schema
+    schema: ({image}) => z.object({
+        title: z.string(),
+        description: z.string(),
+        // Transform string to Date object
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: image().optional(),
+    }),
 });
 
-const projects  = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
+//copy of blog
+const projects = defineCollection({
+    loader: glob({base: './src/content/projects', pattern: '**/*.{md,mdx}'}),
+    schema: ({image}) => z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: image().optional(),
+    }),
 });
 
-export const collections = { blog, projects };
+//copy of blog
+const stormlight = defineCollection({
+    loader: glob({base: './src/content/stormlight', pattern: '**/*.{md,mdx}'}),
+    schema: ({image}) => z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: image().optional(),
+    }),
+});
+
+export const collections = {blog, projects, stormlight};

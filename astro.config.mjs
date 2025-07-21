3fb9ef5a6ec2302e926d0f789d87fd9204e9c0b2
Keane Wong <keanewong913@gmail.com>
2025-07-21 16:15:51 -0700

    Add deployment stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://KeaneWong.com',
+    // base: "satellite-shell",
     integrations: [mdx(), sitemap(), react()],
 });

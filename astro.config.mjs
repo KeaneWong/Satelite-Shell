4aeecc3682effb69fc0de3a2a32c0d05616380a1
Keane Wong <keanewong913@gmail.com>
2025-07-21 21:03:55 -0700

    experimental fixg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -7,7 +7,10 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site:"https://KeaneWong.com",
-    base:"/satellite-shell",
-    integrations: [mdx(), sitemap(), react()]
+    site: "https://KeaneWong.com",
+    base: "/satellite-shell",
+    integrations: [mdx(), sitemap(), react()],
+    build: {
+        assets: 'assets'
+    },
 });

d810b49e9e4a997f3a99249b9d9a75c37cf88b9b
Keane Wong <keanewong913@gmail.com>
2025-07-23 21:19:52 -0700

    Try sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
     },
     image: {
         service: {
-            entrypoint: 'astro/assets/services/noop',
+            entrypoint: 'astro/assets/services/sharp',
         }
     }
 });

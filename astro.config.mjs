ccbd89090d36fd2b6a6001e9541fded8c46843f0
Keane Wong <keanewong913@gmail.com>
2025-07-23 21:32:46 -0700

    ASpecify outdir

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,10 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     site: "https://KeaneWong.com",
-    // base: "/satellite-shell",
+    base: "/satellite-shell",
     trailingSlash: "ignore",
     integrations: [mdx(), sitemap(), react()],
-    base: process.env.NODE_ENV === 'production' ? "/satellite-shell" : "/",
-
+    // base: process.env.NODE_ENV === 'production' ? "/satellite-shell" : "/",
+    output: 'static',
+    outDir: './dist'
 });
